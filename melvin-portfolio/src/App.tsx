3b5b5b9f12c729b9fcd0a54a3902df90ebbd1aea
Melvin González <54681730+melagora@users.noreply.github.com>
2026-02-27 23:27:07 -0600

    Scroll ProgressBar

--- melvin-portfolio/src/App.tsx
+++ melvin-portfolio/src/App.tsx
@@ -4,10 +4,28 @@ import About from "./sections/About";
 import Stack from "./sections/Stack";
 import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
+import { motion, useScroll, useSpring } from "framer-motion";
+
+function ScrollProgress() {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
+  return (
+    <motion.div
+      style={{ scaleX }}
+      className="fixed top-0 left-0 right-0 h-[2px] bg-neutral-900 dark:bg-white origin-left z-[60]"
+    />
+  );
+}
 
 function App() {
   return (
     <>
+      <ScrollProgress />
       <Navbar />
       <main>
         <Hero />
